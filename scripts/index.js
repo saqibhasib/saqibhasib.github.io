5bf1a3dea50075fd99690766f3ffa0c9fd54d560
shasib <saqib_hasib@sfu.ca>
2022-06-03 10:11:16 -0700

    update work

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// const app = document.getElementById("app-id");
+// var title = `<div class="container">about</div>`;
+
+// const work = document.getElementById("work-body");
+
+// console.log(work)
+// app.innerHTML = work.firstChild;
+// app.textContent = title;
+
+// window.addEventListener("load", function() {
+//     document.getElementsByTagName("html")[0].replaceChild(about.getElementsByTagName("div")[0], document.getElementsByTagName("div")[0]);
+// }, false);
